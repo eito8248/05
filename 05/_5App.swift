451c000e0beb66a1d9ad8fa8104617b077b44bc5
eito8248 <eimaru.248@icloud.com>
2025-05-21 10:47:43 +0900

    Initial Commit

---
+++ 05/_5App.swift
@@ -0,0 +1,17 @@
+//
+//  _5App.swift
+//  05
+//
+//  Created by Tamura Lab on 2025/05/21.
+//
+
+import SwiftUI
+
+@main
+struct _5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 05Tests/_5Tests.swift
@@ -0,0 +1,17 @@
+//
+//  _5Tests.swift
+//  05Tests
+//
+//  Created by Tamura Lab on 2025/05/21.
+//
+
+import Testing
+@testable import _5
+
+struct _5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
